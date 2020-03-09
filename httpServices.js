f042c622fe11ca4920d5ab8a7ea79bdf958f1794
bob <chuvanan29081998@gmail.com>
2020-03-09 16:45:19 +0700

    create

---
+++ httpServices.js
@@ -0,0 +1,28 @@
+
+async function get(url) {
+    let options = {
+        method: 'get',
+        headers: {
+            'Accept': 'Application/json',
+            'Content-type': 'Application/json',
+        }
+    }
+    let a = []
+    return await fetch(url, options)
+        .then(res => res.json())
+
+}
+async function post(url, data) {
+
+    let options = {
+        method: 'get',
+        headers: {
+            'Accept': 'Application/json',
+            'Content-type': 'Application/json',
+        },
+        body: JSON.stringify(data)
+    }
+    return await fetch(url, options)
+        .then(res => res.json())
+}
+
